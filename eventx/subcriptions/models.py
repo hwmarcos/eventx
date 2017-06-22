77482b278e0804abc4087dc07f590dfc1933d65d
helder w marcos <helderwmarcos@gmail.com>
2017-06-22 20:32:25 -0300

    Simplica com ModelForm

--- eventx/subcriptions/models.py
+++ eventx/subcriptions/models.py
@@ -1,11 +1,13 @@
 from django.db import models
+from eventx.subcriptions.validators import validate_cpf
+
 
 class Subscription(models.Model):
-    name = models.CharField(max_length=100)
-    cpf = models.CharField(max_length=11)
-    email = models.EmailField()
-    phone = models.CharField(max_length=20)
-    created_at = models.DateTimeField(auto_now_add=True)
+    name = models.CharField('name', max_length=100, blank=True)
+    cpf = models.CharField('cpf', max_length=11, validators=[validate_cpf])
+    email = models.EmailField('email', blank=True)
+    phone = models.CharField('phone', blank=True, max_length=20)
+    created_at = models.DateTimeField('cadastrado em', auto_now_add=True)
     paid = models.BooleanField('pago', default=False)
 
     class Meta:
