141b4398e0101425bbde4294a9b639668d0efdf0
helder w marcos <helderwmarcos@gmail.com>
2017-06-24 19:17:42 -0300

    Speakers

--- eventx/core/tests/test_view_home.py
+++ eventx/core/tests/test_view_home.py
@@ -3,6 +3,8 @@ from django.shortcuts import resolve_url as r
 
 class HomeTest(TestCase):
 
+    fixtures = ['keynotes.json']
+
     def setUp(self):
         self.response = self.client.get(r('home'))
 
@@ -22,7 +24,9 @@ class HomeTest(TestCase):
             'Grace Hopper',
             'http://hbn.link/hopper-pic',
             'Alan Turing',
-            'http://hbn.link/turing-pic'
+            'http://hbn.link/turing-pic',
+            'href="{}"'.format(r('speaker_detail', slug='grace-hopper')),
+            'href="{}"'.format(r('speaker_detail', slug='alan-turing')),
         ]
         for expected in contents:
             with self.subTest():
--- eventx/urls.py
+++ eventx/urls.py
@@ -1,9 +1,10 @@
 from django.conf.urls import include, url
 from django.contrib import admin
-from eventx.core.views import home
+from eventx.core.views import home, speaker_detail
 
 urlpatterns = [
     url(r'^$', home, name='home'),
     url(r'inscricao/', include('eventx.subcriptions.urls', namespace='subscriptions')),
+    url(r'^palestrantes/(?P<slug>[\w-]+)/$', speaker_detail, name='speaker_detail'),
     url(r'^admin/', include(admin.site.urls)),
 ]
